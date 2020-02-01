4a9a2799f26484163d03a90d2c2ac691c79025da
Anthony DelSordo <anthonydelsordo@yahoo.com>
2020-02-01 14:44:19 -0500

    hooked up interaction to inventory

--- Assets/Scripts/PlayerInteraction.cs
+++ Assets/Scripts/PlayerInteraction.cs
@@ -3,24 +3,35 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerInteraction : MonoBehaviour {
-    private GameObject playerCamera;
-    GameObject interactedObject;
+    GameObject playerCamera;
+    [SerializeField] GameObject interactedObject;
     [SerializeField] int playerReach = 4;
-    [SerializeField] LayerMask layerMask;
+    [SerializeField] LayerMask interactionLayerMask;
+    [SerializeField] PlayerInventory playerInventory;
 
     void Start() {
         playerCamera = GameObject.FindGameObjectWithTag("MainCamera");
+        playerInventory = GetComponent<PlayerInventory>();
     }
 
     void Update() {
         RaycastHit hit;
 
-        if (Physics.Raycast(playerCamera.transform.position, playerCamera.transform.TransformDirection(Vector3.forward), out hit, playerReach, layerMask)) {
+        if (Physics.Raycast(playerCamera.transform.position, playerCamera.transform.TransformDirection(Vector3.forward), out hit, playerReach, interactionLayerMask)) {
             interactedObject = hit.collider.gameObject;
             Debug.DrawRay(playerCamera.transform.position, playerCamera.transform.TransformDirection(Vector3.forward) * playerReach, Color.green);
         } else {
             interactedObject = null;
             Debug.DrawRay(playerCamera.transform.position, playerCamera.transform.TransformDirection(Vector3.forward) * playerReach, Color.white);
+        }
+
+        if (Input.GetButtonDown("Interaction") && interactedObject != null) {
+            bool canPickUp = true;
+            string pickedUpItem = "crowbar";
+
+            if(canPickUp) {
+                playerInventory.itemUpdated(pickedUpItem);
+            }
         }
     }
 }
--- Assets/Scripts/PlayerInventory.cs
+++ Assets/Scripts/PlayerInventory.cs
@@ -2,17 +2,40 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class InventoryScript : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+public class PlayerInventory : MonoBehaviour {
+    [SerializeField] bool crowbar;
+    [SerializeField] bool gasCan;
+    [SerializeField] bool candle;
+    [SerializeField] bool lantern;
+    [SerializeField] bool sparkPlugs;
+    [SerializeField] bool ductTape;
+    [SerializeField] int match;
+
+    void Start() {
+        crowbar = false;
+        gasCan = false;
+        candle = false;
+        lantern = false;
+        sparkPlugs = false;
+        ductTape = false;
+        match = 5;
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    public void itemUpdated(string item) {
+        if (item == "crowbar") {
+            this.crowbar = true;
+        } else if (item == "gasCan") {
+            this.gasCan = true;
+        } else if (item == "candle") {
+            this.candle = true;
+        } else if (item == "lantern") {
+            this.lantern = true;
+        } else if (item == "sparkPlugs") {
+            this.sparkPlugs = true;
+        } else if (item == "ductTape") {
+            this.ductTape = true;
+        } else if (item == "match") {
+            this.match++;
+        }
     }
 }
