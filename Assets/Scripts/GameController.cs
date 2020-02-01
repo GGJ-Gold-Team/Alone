1df22e0bc017a6ff59606e292fa4544ee1550015
Tyler Batts <tyler_batts@yahoo.com>
2020-02-01 11:46:22 -0500

    added pause menu

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour {
+    [SerializeField]
+    GameState gameState = null;
+
+    bool isPaused = false;
+
+    void Start() {
+        gameState = new GameState();
+    }
+
+    void Update() {
+        if (Input.GetButtonDown("Pause")) {
+            if (isPaused) {
+                Unpause();
+            } else {
+                Pause();
+            }
+        }
+    }
+
+    public void Pause() {
+        isPaused = true;
+        Time.timeScale = 0;
+    }
+
+    public void Unpause() {
+        isPaused = false;
+        Time.timeScale = 1;
+    }
+
+    public bool IsPaused {
+        get {
+            return isPaused;
+        }
+    }
+}
---
+++ Assets/Scripts/PauseMenuController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseMenuController : MonoBehaviour {
+    [SerializeField]
+    Canvas canvas;
+
+    [SerializeField]
+    GameController gameController;
+
+    void Start() {
+        gameController = GameObject.FindGameObjectWithTag("GameController").GetComponent<GameController>();
+    }
+
+    void Update() {
+        canvas.enabled = (gameController && gameController.IsPaused);
+    }
+}
