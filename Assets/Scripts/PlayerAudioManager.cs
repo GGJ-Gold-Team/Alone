0000d74a864520bc9f54705d0e960bc8e400bbc7
Tyler <tyler_batts@yahoo.com>
2020-02-02 11:38:30 -0500

    added walking audio clip

---
+++ Assets/Scripts/PlayerAudioManager.cs
@@ -0,0 +1,46 @@
+using UnityEngine;
+
+public class PlayerAudioManager : MonoBehaviour {
+    [SerializeField] AudioSource walkingSnowAudioSource;
+    [SerializeField] AudioSource walkingCabinAudioSource;
+
+    [SerializeField] LayerMask cabinLayer;
+    [SerializeField] LayerMask terrainLayer;
+
+    bool isPlaying;
+    float magnitude;
+    Rigidbody rigidbody;
+
+    void Start() {
+        rigidbody = GetComponent<Rigidbody>();
+    }
+
+    void Update() {
+        isPlaying = walkingCabinAudioSource.isPlaying || walkingSnowAudioSource.isPlaying;
+        magnitude = Vector3.Magnitude(rigidbody.velocity);
+
+        if (magnitude > 0.5f) {
+            PlayWalkAudio();
+        } else {
+            StopWalkAudio();
+        }
+    }
+
+    void PlayWalkAudio() {
+        if (!isPlaying) {
+            AudioSource source = walkingSnowAudioSource;
+            RaycastHit cabinHit;
+            if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.down), out cabinHit, 2f, cabinLayer)) {
+                Debug.Log("Cabin Floor Hit");
+                source = walkingCabinAudioSource;
+            }
+
+            source.Play();
+        }
+    }
+
+    void StopWalkAudio() {
+        walkingCabinAudioSource.Stop();
+        walkingSnowAudioSource.Stop();
+    }
+}
