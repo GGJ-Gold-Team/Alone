af92fdead54964aed75ef4d25eccf61cea0e2c2f
Anthony DelSordo <anthonydelsordo@yahoo.com>
2020-02-02 17:12:59 -0500

    added menu

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -5,11 +5,22 @@ using UnityEngine;
 public class GameController : MonoBehaviour {
     [SerializeField]
     GameState gameState = null;
+    [SerializeField]
+    GameObject[] lights;
+    [SerializeField]
+    GameObject playerObject;
 
     bool isPaused = false;
 
     void Start() {
         gameState = new GameState();
+        playerObject = GameObject.FindGameObjectWithTag("Player");
+        playerObject.GetComponent<PlayerScript>().onToggleMenuCallback = onMenuToggle;
+        playerObject.GetComponent<PlayerScript>().onToggleMenu(true);
+        lights = GameObject.FindGameObjectsWithTag("SafeZone");
+        for (int i = 0; i < lights.Length; i++) {
+            lights[i].GetComponentInChildren<FireLightSource>().toggleInfinite(true);
+        }
     }
 
     void Update() {
@@ -18,6 +29,16 @@ public class GameController : MonoBehaviour {
                 Unpause();
             } else {
                 Pause();
+            }
+        }
+    }
+
+    void onMenuToggle(bool isMenuOpen) {
+        Debug.Log("gameController: " + isMenuOpen);
+        if (!isMenuOpen) {
+            for (int i = 0; i < lights.Length; i++) {
+                lights[i].GetComponentInChildren<FireLightSource>().toggleInfinite(false);
+                lights[i].GetComponentInChildren<FireLightSource>().onToggleLightSource(false);
             }
         }
     }
